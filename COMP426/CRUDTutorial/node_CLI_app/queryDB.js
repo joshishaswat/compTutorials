18db9e0ad647ef7f7a36c066a728f40a59741b40
joshishaswat <joshishaswat@gmail.com>
2024-06-03 21:22:20 -0400

    Added CRUD tutorial

---
+++ COMP426/CRUDTutorial/node_CLI_app/queryDB.js
@@ -0,0 +1,30 @@
+import fs from "fs"
+
+export default async function queryDB(externalFunction) {
+	try{
+		let info = []
+
+		if(fs.existsSync("db.json")) {
+			await fs.readFile("db.json", function(err, data) {
+				info = JSON.parse(data.toString())
+				console.log(info)
+
+				if(err) {
+					console.log(err)
+				}
+
+				if(externalFunction && !err) {
+					externalFunction(info)
+					return
+				}
+			})
+		} else {
+			if (externalFunction) {
+				externalFunction(info)
+				return
+			}
+		} 
+	} catch(error) {
+			console.log(`Something Happened: ${error.message}`)
+		}
+}
