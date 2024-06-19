259c9363782c0037175fd8b220570fd15f44a411
joshishaswat <joshishaswat@gmail.com>
2024-06-18 23:15:08 -0400

    added files to authorization tutorial

--- COMP426/AuthorizationTutorial/JWT/app.js
+++ COMP426/AuthorizationTutorial/JWT/app.js
@@ -1,8 +1,22 @@
 const express = require("express"),
-app = express()
+app = express(),
+mongoose = require("mongoose"),
+userRoutes = require("./routes/user")
 
-require("dotenv")
-	.config()
+// Connect to database
+try {
+	mongoose.connect("mongodb+srv://joshishaswat:Samosabucket@cluster0.kgylxvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+	})
+	console.log("connected to db")
+} catch (error) {
+	handleError(error)
+}
+
+process.on('unhandleRejection', error => {
+	console.log('unhandleRejection', error.message)
+})
 
 // Parse requests of content-type - application/json
 app.use(express.json())
@@ -10,6 +24,9 @@ app.use(express.json())
 app.use(express.urlencoded({
 	extended: true
 }))
+
+//using user route
+app.use(userRoutes)
 
 // Setup server to listen on port 8080
 app.listen(process.env.PORT || 8080, () => {
